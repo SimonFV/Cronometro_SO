1bcf1b071f7dfe0e24d1393649449a8ff557f661
Simón Fallas <47420204+SimonFV@users.noreply.github.com>
2023-08-16 19:16:09 -0600

    init

---
+++ software/cronometro/obj/default/hello_world_small.d
@@ -0,0 +1,19 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\sys\alt_irq.h \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\nios2.h \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\alt_types.h \
+ c:/intelFPGA_lite/Proyectos/tutorial/software/cronometro_bsp/system.h \
+ c:/intelFPGA_lite/Proyectos/tutorial/software/cronometro_bsp/linker.h \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\io.h \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\alt_types.h \
+ c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\drivers\inc\altera_avalon_timer_regs.h
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\sys\alt_irq.h:
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\nios2.h:
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\alt_types.h:
+c:/intelFPGA_lite/Proyectos/tutorial/software/cronometro_bsp/system.h:
+c:/intelFPGA_lite/Proyectos/tutorial/software/cronometro_bsp/linker.h:
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\io.h:
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\hal\inc\alt_types.h:
+c:\intelfpga_lite\proyectos\tutorial\software\cronometro_bsp\drivers\inc\altera_avalon_timer_regs.h:
